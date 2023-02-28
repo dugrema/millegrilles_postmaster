5f05df9e7f53dd4acfab845a8c82b76992717dda
Mathieu Dugre <mathieu.dugre@mdugre.info>
2023-02-28 12:21:27 -0500

    Recuperation erreurs smtp

--- src/email.rs
+++ src/email.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use lettre::transport::smtp::Error as ErrorLettre;
 use log::{debug, error, warn};
 
 use lettre::{
@@ -50,84 +51,31 @@ pub async fn post_email(gestionnaire: &GestionnairePostmaster, email: EmailNotif
     let mailer = AsyncSmtpTransport::<Tokio1Executor>::relay(smtp.hostname.as_str())?
         .credentials(smtp_credentials)
         .port(smtp.port)
-        // .tls(Tls::Required(tls_parameters))
         .build();
 
-    let from = email_from.as_str();
-    let to = email.adress.as_str();
-    let subject = email.title.as_str();
-    let body = email.body;
+    let email_formatted = format_email(email_from.as_str(), email)?;
 
-    match send_email_smtp(&mailer, from, to, subject, body).await {
-        Ok(r) => {
-            debug!("SMTP send ok : {:?}", r);
-        },
-        Err(e) => {
-            error!("Erreur traitement smtp : {:?}", e);
+    debug!("post_email send_email_smtp  {:?}", email_formatted);
+    if let Err(e) = mailer.send(email_formatted).await {
+        error!("post_email Erreur traitement smtp : {:?}", e);
+        if e.is_permanent() {
+            warn!("email.post_email Erreur permanente : {:?}", e.source());
+            panic!("Erreur permanente - Desactiver notifications email");
+        } else if e.is_transient() || e.is_timeout() {
+            panic!("Creer periode d'attente emission email");
+        } else {
+            panic!("Erreur autre - non geree");
         }
     }
 
     Ok(())
 }
 
-async fn send_email_smtp(
-    mailer: &AsyncSmtpTransport<Tokio1Executor>,
-    from: &str,
-    to: &str,
-    subject: &str,
-    body: String,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn format_email(from: &str, email: EmailNotification) -> Result<Message, Box<dyn Error>> {
     let email = Message::builder()
         .from(from.parse()?)
-        .to(to.parse()?)
-        .subject(subject)
-        .body(body.to_string())?;
-
-    debug!("send_email_smtp  {:?}", email);
-
-    mailer.send(email).await?;
-
-    Ok(())
+        .to(email.adress.parse()?)
+        .subject(email.title.as_str())
+        .body(email.body)?;
+    Ok(email)
 }
-
-// async fn post_email<S>(gestionnaire: &GestionnairePostmaster, nom_domaine: S) -> Result<(), Box<dyn Error>>
-//     where S: Into<String>
-// {
-//     // Creer contexte - monter dans le gestionnaire pour reutilisation
-//     let nom_domaine = nom_domaine.into();
-//     let domaine = Domain::from_unchecked(nom_domaine.clone());
-//     let ctx = simple_context::new(domaine, "unique_ctx123".parse().unwrap()).unwrap();
-//
-//     // don't use unencrypted con for anything but testing and
-//     // simplified examples
-//     let (smtp, email_from) = {
-//         let guard = gestionnaire.configuration_notifications.lock()?.expect("lock");
-//         let email_from = match guard.email_from.as_ref() {
-//             Some(inner) => inner.to_owned(),
-//             None => Err(format!("Email from manquant"))?
-//         };
-//         let smtp = match guard.smtp.as_ref() {
-//             Some(inner) => inner.clone(),
-//             None => Err(format!("Aucune configuration smtp - abort"))?
-//         };
-//         (smtp, email_from)
-//     };
-//
-//     let mut mail = Mail::plain_text("Some body", &ctx);
-//     mail.insert_headers(headers! {
-//         _From: ["bla@example.com"],
-//         _To: ["blub@example.com"],
-//         Subject: "Some Mail"
-//     }.unwrap());
-//
-//     let con_config = ConnectionConfig::builder_local_unencrypted().build();
-//     // let con_config = ConnectionConfig::builder_with_port(domaine.parse()?)?
-//     //     .auth()
-//     //     .build();
-//
-//     let fut = smtp::send(mail.into(), con_config, ctx);
-//     fut.await?;
-//
-//
-//     Ok(())
-// }
